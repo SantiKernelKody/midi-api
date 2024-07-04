e283384abb0ffde5c1182a5eb981e3c0ebe65be8
santiago16171 <saantumb@espol.edu.ec>
2024-07-04 14:01:31 -0500

    security

---
+++ core/config.py
@@ -0,0 +1,11 @@
+# app/core/config.py
+import os
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+settings = Settings()
