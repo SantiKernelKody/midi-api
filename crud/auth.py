40743ec93cf9cf2e49ad2cf1f09f0aec019bd9c7
santiago16171 <saantumb@espol.edu.ec>
2024-07-05 09:19:12 -0500

    player crud

--- crud/auth.py
+++ crud/auth.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 from models.dashboard_user import DashboardUser
 from models.player import Player
-from schemas.dashboard_user import DashboardUserCreate, DashboardUserUpdate
 from utils.auth import verify_password
 
-def authenticate_user(db: Session, email: str, password: str):
+
+def authenticate_user(db: Session, email: str, password: str) -> DashboardUser | bool:
     user = db.query(DashboardUser).filter(DashboardUser.email == email).first()
     if not user:
         return False
--- crud/player.py
+++ crud/player.py
@@ -1,8 +1,25 @@
 from sqlalchemy.orm import Session
 from models.player import Player
+from models.player_level import PlayerLevel
+from schemas.player_level import PlayerLevelCreate
+from models.player_story import PlayerStory
+from schemas.player_story import PlayerStoryCreate
 
-def find_player_by_name(db: Session, name: str):
+def find_player_by_name(db: Session, name: str) -> Player | None:
     player = db.query(Player).filter(Player.name == name).first()
     if not player:
         return None
-    return player.id
+    return player
+def create_player_level(db: Session, player_level: PlayerLevelCreate):
+    db_player_level = PlayerLevel(**player_level.dict())
+    db.add(db_player_level)
+    db.commit()
+    db.refresh(db_player_level)
+    return db_player_level
+
+def create_player_story(db: Session, player_story: PlayerStoryCreate):
+    db_player_story = PlayerStory(**player_story.dict())
+    db.add(db_player_story)
+    db.commit()
+    db.refresh(db_player_story)
+    return db_player_story
