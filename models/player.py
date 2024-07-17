ebe2af7713f1231d66342f4aad900cdc12970e7e
santiago16171 <saantumb@espol.edu.ec>
2024-07-17 17:56:15 -0500

    game enpoint changes

--- models/player.py
+++ models/player.py
@@ -1,19 +1,21 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, TIMESTAMP
+from sqlalchemy.orm import relationship
 from db.base_class import Base
-from datetime import datetime
 
 class Player(Base):
     __tablename__ = "player"
+
     id = Column(Integer, primary_key=True, index=True)
-    school_id = Column(Integer)
-    avatar_id = Column(Integer)
-    special_need_id = Column(Integer)
-    name = Column(String(128))
-    last_name = Column(String(128))
-    edad = Column(Integer)
-    ethnicity = Column(String(32))
-    special_need_description = Column(String(255))
-    special_need = Column(Boolean)
-    user_name = Column(String(255))
-    password = Column(String(255))
-    created_at = Column(DateTime, default=datetime.utcnow)
+    school_id = Column(Integer, ForeignKey("educational_entity.id"))
+    special_need_id = Column(Integer, ForeignKey("special_need.id"))
+    full_name = Column(String(128), nullable=False)
+    edad = Column(Integer, nullable=True)
+    ethnicity = Column(String(32), nullable=True)
+    special_need_description = Column(String(255), nullable=True)
+    special_need = Column(Boolean, nullable=True)
+    user_name = Column(String(255), nullable=True)
+    password = Column(String(255), nullable=True)
+    created_at = Column(TIMESTAMP, nullable=True)
+
+    school = relationship("EducationalEntity")
+    special_need = relationship("SpecialNeed")
--- schemas/player.py
+++ schemas/player.py
@@ -1,39 +1,29 @@
 from pydantic import BaseModel
-from typing import Optional
 from datetime import datetime
 
 class PlayerBase(BaseModel):
-    name: str
-    last_name: str
-    edad: int
-    ethnicity: str
-    special_need_description: Optional[str] = None
-    special_need: bool
-    user_name: str
-    password: str
+    school_id: int
+    special_need_id: int
+    full_name: str
+    edad: int = None
+    ethnicity: str = None
+    special_need_description: str = None
+    special_need: bool = None
+    user_name: str = None
+    password: str = None
 
 class PlayerCreate(PlayerBase):
-    school_id: int
-    avatar_id: Optional[int] = None
-    special_need_id: Optional[int] = None
+    pass
 
 class PlayerUpdate(PlayerBase):
-    school_id: Optional[int] = None
-    avatar_id: Optional[int] = None
-    special_need_id: Optional[int] = None
+    pass
 
 class PlayerInDBBase(PlayerBase):
     id: int
-    school_id: int
-    avatar_id: Optional[int] = None
-    special_need_id: Optional[int] = None
-    created_at: datetime
+    created_at: datetime.datetime
 
     class Config:
         orm_mode = True
 
 class Player(PlayerInDBBase):
     pass
-
-class PlayerInDB(PlayerInDBBase):
-    hashed_password: str
