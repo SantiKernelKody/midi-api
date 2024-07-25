b8f7622eadef871eecc0f092f333640b48f5828c
santiago16171 <saantumb@espol.edu.ec>
2024-07-24 23:14:19 -0500

    token

---
+++ utils/token.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from typing import Union
+from jose import JWTError, jwt
+from core.config import settings
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = "HS256"
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
